090b7220ce12dad892fda89d3cd9cc338fcb20bb
pooyadehghan@yahoo.com <pooyadehghan@yahoo.com>
2022-06-30 22:16:05 +0430

    added migration via npx sequelize-cli command

---
+++ migrations/20220629213929-create-recipe.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Recipes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      ingredients: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      direction: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER.UNSIGNED,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Recipes');
+  }
+};
