ec353a9289f8c63490e7d38db96cd47b6013eed4
pooyadehghan@yahoo.com <pooyadehghan@yahoo.com>
2022-07-12 20:13:55 +0430

    one new model and thats recipe!

--- models/recipe.js
+++ models/recipe.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Recipe.belongsTo(models.User , {foreignKey : userId})
+      Recipe.belongsTo(models.User , {foreignKey : 'userId'})
     }
   }
   Recipe.init({
